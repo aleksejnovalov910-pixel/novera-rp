f0165e61daf1138f6a0a30bfd8ad93d59bbfd4c8
aleksejnovalov910-pixel <aleksejnovalov910@gmail.com>
2026-08-20 20:39:16 +0300

    chore: add playable system manifest

---
+++ packages/novera/manifest.js
@@ -0,0 +1,2 @@
+'use strict';
+module.exports={name:'NOVERA RP',version:'0.18.0-dev',stage:'playable-baseline',systems:['auth-integration','characters','spawn','economy','inventory','jobs','vehicles','properties','phone','tablet','settings','organizations','families','V-Market','tutorial','admin-logging']};
