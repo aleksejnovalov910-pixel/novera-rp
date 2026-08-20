f10098364176040a070c45e03caed3f9fdf957b4
aleksejnovalov910-pixel <aleksejnovalov910@gmail.com>
2026-08-20 20:30:30 +0300

    feat: centralize NOVERA playable configuration

---
+++ packages/novera/config.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports={
+ version:'0.18.0-dev',
+ maxPlayers:500,
+ starterCash:5000,
+ starterBank:15000,
+ spawn:{x:215.76,y:-810.12,z:30.73},
+ age:{min:18,max:90},
+ characterSlots:3,
+ keys:{phone:'ARROW_UP',tablet:'ARROW_DOWN',settings:'F2'},
+ marketName:'V-Market'
+};
